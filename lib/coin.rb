e4b3820685e9138474d1a9820bcdb70d482ffb59
Tatiana Soukiassian <binaryberry@gmail.com>
2014-11-26 22:38:11 +0000

    built coin factory

--- lib/coin.rb
+++ lib/coin.rb
@@ -4,7 +4,41 @@ class Coin
 
 	def initialize(value, name)
 		@value = value
-		@name = "#{name}"
+		@name = name
 	end
+
+
+	def self.one_pence
+		new(0.01, "1p")
+	end
+
+	def self.two_pence
+		new(0.02, "2p")
+	end
+
+	def self.five_pence
+		new(0.05, "5p")
+	end
+
+	def self.ten_pence
+		new(0.10, "10p")
+	end
+
+	def self.twenty_pence
+		new(0.20, "20p")
+	end
+
+	def self.fifty_pence
+		new(0.50, "50p")
+	end
+
+	def self.one_pound
+		new(1, "£1")
+	end
+
+	def self.two_pounds
+		new(2, "£2")
+	end
+	
 
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -12,4 +12,55 @@ describe Coin do
 		expect(coin.name).to eq "penny"
 	end
 
+	it "can be 1p" do
+		coin = Coin.one_pence
+		expect(coin.value).to eq 0.01
+		expect(coin.name).to eq "1p"
+	end
+
+	it "can be 2p" do
+		coin = Coin.two_pence
+		expect(coin.value).to eq 0.02
+		expect(coin.name).to eq "2p"
+	end
+
+	it "can be 5p" do
+		coin = Coin.five_pence
+		expect(coin.value).to eq 0.05
+		expect(coin.name).to eq "5p"
+	end
+
+	it "can be 10p" do
+		coin = Coin.ten_pence
+		expect(coin.value).to eq 0.1
+		expect(coin.name).to eq "10p"
+	end
+
+	it "can be 20p" do
+		coin = Coin.twenty_pence
+		expect(coin.value).to eq 0.20
+		expect(coin.name).to eq "20p"
+	end
+
+	it "can be 50p" do
+		coin = Coin.fifty_pence
+		expect(coin.value).to eq 0.50
+		expect(coin.name).to eq "50p"
+	end
+
+	it "can be £1" do
+		coin = Coin.one_pound
+		expect(coin.value).to eq 1
+		expect(coin.name).to eq "£1"
+	end
+
+	it "can be £2" do
+		coin = Coin.two_pounds
+		expect(coin.value).to eq 2
+		expect(coin.name).to eq "£2"
+	end
+
+
+	# it "cannot be a value other than 1p, 2p, 5p, 10p, 20p, 50p, £1, £2" do
+	# end
 end
